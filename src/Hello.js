c88a38b643f6b9feec6e694b2dfe217e74210271
shanacaroti <shana.caroti@students.fhnw.ch>
2021-07-15 11:10:49 +0200

    add robots-file and create card component

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './Hello.css';
 
-class Hello extends Component {
-	render() {
+const Hello = (props) => {
+	
 		return (
 			<div className='f1 tc'> 
 				<h1> Hello World</h1>
-				<p>{this.props.greeting}</p>
+				<p>{props.greeting}</p>
 			</div>
 			
 		);
-	}
+	
 }
 
 export default Hello;
